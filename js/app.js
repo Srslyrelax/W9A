1e78192d1a9d14cb4faadd8dcaa9a1f96b33403b
Srslyrelax <srlsyrelax@gmail.com>
2020-11-12 19:48:28 -0700

    added files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var age = "40"
+var is_subscribed = false
+
+if (is_subscribed == true && age < 18) {
+    console.log("User is Subscribed and Under 18")
+}
+else if (is_subscribed == true && age > 18) {
+    console.log("User is Subscribed and Over 18")
+}
+else if (is_subscribed == false && age > 18) {
+    console.log("User is not Subscribed and over 18")
+}
+else if (is_subscribed == false && age < 18) {
+    console.log("User is not Subscribed and under 18")
+}
+
+else alert("script go boom")
