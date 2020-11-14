ec0c6e73b3964d4210f84d2fb902721d6330b38d
Srslyrelax <srlsyrelax@gmail.com>
2020-11-14 15:37:59 -0700

    completed W9B

--- js/app.js
+++ js/app.js
@@ -1,71 +1,19 @@
-var age = "40"
-var is_subscribed = false
+var age = [6, 8, 10, 15, 43, 47, 55, 79, 60];
+var is_subscribed = [true, true, true, false, false, true, false, true, false];
 
-if (is_subscribed == true && age < 18) {
-    console.log("User is Subscribed and Under 18")
-}
-else if (is_subscribed == true && age > 18) {
-    console.log("User is Subscribed and Over 18")
-}
-else if (is_subscribed == false && age > 18) {
-    console.log("User is not Subscribed and over 18")
-}
-else if (is_subscribed == false && age < 18) {
-    console.log("User is not Subscribed and under 18")
-}
-
+for (var i = 0; i < age.length; i++) {
+    if (is_subscribed[i] == true && age[i] < 18) {
+        console.log("User is Subscribed and Under 18")
+    }
+    else if (is_subscribed[i] == true && age[i] > 18) {
+        console.log("User is Subscribed and Over 18")
+    }
+    else if (is_subscribed[i] == false && age[i] > 18) {
+        console.log("User is not Subscribed and over 18")
+    }
+    else if (is_subscribed[i] == false && age[i] < 18) {
+        console.log("User is not Subscribed and under 18")
+    } 
 else alert("script go boom")
-
-var age = "40"
-var is_subscribed = true
-
-if (is_subscribed == true && age < 18) {
-    console.log("User is Subscribed and Under 18")
-}
-else if (is_subscribed == true && age > 18) {
-    console.log("User is Subscribed and Over 18")
-}
-else if (is_subscribed == false && age > 18) {
-    console.log("User is not Subscribed and over 18")
-}
-else if (is_subscribed == false && age < 18) {
-    console.log("User is not Subscribed and under 18")
 }
 
-else alert("script go boom")
-
-var age = "15"
-var is_subscribed = false
-
-if (is_subscribed == true && age < 18) {
-    console.log("User is Subscribed and Under 18")
-}
-else if (is_subscribed == true && age > 18) {
-    console.log("User is Subscribed and Over 18")
-}
-else if (is_subscribed == false && age > 18) {
-    console.log("User is not Subscribed and over 18")
-}
-else if (is_subscribed == false && age < 18) {
-    console.log("User is not Subscribed and under 18")
-}
-
-else alert("script go boom")
-
-var age = "15"
-var is_subscribed = true
-
-if (is_subscribed == true && age < 18) {
-    console.log("User is Subscribed and Under 18")
-}
-else if (is_subscribed == true && age > 18) {
-    console.log("User is Subscribed and Over 18")
-}
-else if (is_subscribed == false && age > 18) {
-    console.log("User is not Subscribed and over 18")
-}
-else if (is_subscribed == false && age < 18) {
-    console.log("User is not Subscribed and under 18")
-}
-
-else alert("script go boom")
